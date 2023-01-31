dbbd94847fe97df2ca2c3b7311832ac8bfae6e05
bharat <bharat132000@gmail.com>
2023-02-01 02:49:32 +0530

    Initial Commit

---
+++ app/src/main/java/com/project/keepy/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.project.keepy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.keepy.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
